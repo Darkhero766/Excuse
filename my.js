a245c450e2fa57b45bf8d2dda526cc7bca8b8fdd
Faizal <nightowlclub72@gmail.com>
2026-06-16 10:52:40 +0000

    added some js tooo

---
+++ my.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("button");
+
+const box = document.getElementById("box");
+
+button.addEventListener("click", function () {
+    box.innerText = "My dog pooped on keyboard so can't type code rn"
+})
